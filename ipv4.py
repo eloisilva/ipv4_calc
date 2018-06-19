649d3b496aabcd26910d6a6b8681ff77f73695fa
Eloi Silva <etch.linux@gmail.com>
2018-06-19 15:56:22 -0300

    Add comment to document functions

--- ipv4.py
+++ ipv4.py
@@ -4,7 +4,7 @@
 #     Version             :     V4
 #     Created By          :     Eloi Silva (etch.linux@gmail.com)
 #     Creation Date       :     [2018-05-21 19:36]
-#     Last Modified       :     [2018-06-18 19:09]
+#     Last Modified       :     [2018-06-19 15:38]
 #     Description         :      
 #################################################################################
 
@@ -12,8 +12,13 @@ import sys
 
 def ip_calc(ip):
     '''
-    ip: receive a ipv4/cidr as argument
-    Return: ip, mask, network and broadcast in binary format
+    Arg:
+        ip: receive a ipv4/cidr as argument
+    Return a tupple with:
+        IP string binary notation
+        Mask string binary notation
+        Network string binary notation
+        Broadcast string binary notation
     '''
     ip, mask = ip_check(ip)
     network = network_calc(ip, mask)
@@ -23,26 +28,42 @@ def ip_calc(ip):
 def ip_check(ip):
     '''
     Check if it is a valid IPv4 address
+    Arg:
+        ip: Receive string ip/cidr
     Return:
-      A list of IPv4 octet
-      CIDR Mask integer
+        IPv4 String binary notation
+        Mask String binary notation
     '''
     try:
         # Check IPv4 blocks and mask and return ip octet blocks and CIDR mask
         ip_blocks = [block for block in map(ip_oct_check, ip.rstrip().lstrip().split('/')[0].split('.')) if block]
         mask = int(ip.split('/')[1]) if len(ip.split('/')) == 2 else 32
         if len(ip_blocks) == 4:
-            return ip_bin(ip_blocks), convert_int_to_bits(mask)
+            return ip_blocks_to_bin(ip_blocks), convert_int_to_bits(mask)
         else:
             raise ValueError("Value is not a IPv4")
     except Exception:
         sys.stderr.write('Invalid IPv4: %s' % ip)
         return None
 
-def ip_bin(ip):
+def ip_blocks_to_bin(ip):
+    '''
+    Receive a list of integers and convert it to binary representation
+    Arg:
+        A list of integer number
+    Return:
+        String bits of the list
+    '''
     return ''.join([b for b in map(convert_decimal_to_bits, ip)])
 
 def ip_oct_check(octeto):
+    '''
+    Checks if octet number is between 0 to 255
+    Arg:
+        A integer number from 0 to 255
+    Return:
+        The integer number
+    '''
     octeto = int(octeto)
     if octeto < 256 and octeto >= 0:
         return octeto
@@ -50,6 +71,15 @@ def ip_oct_check(octeto):
         return False
 
 def convert_int_to_bits(bits, bits_total=32):
+    '''
+    Convert integer numbers from 0 to 32 in string binary notation.
+    Used to convert mask CIDR notation in network mask
+    Arg:
+        bits: String of bits
+        bits_total: Max number of bits expected
+    Return:
+        Network mask bits
+    '''
     bits = bits * '1'
     if len(bits) < bits_total:
         bits0 = bits_total - len(bits)
@@ -57,13 +87,29 @@ def convert_int_to_bits(bits, bits_total=32):
     return bits
 
 def convert_decimal_to_bits(decimal, bits_total=8):
-    decimal = bin(decimal)[2:]
+    '''
+    Convert ip actet (int) to binary notation
+    Arg:
+        decimal: A integer number
+        bits_total: Max number of bits expected
+    Return:
+        String bits of the list
+    '''
+    decimal = bin(int(decimal))[2:]
     if len(decimal) < bits_total:
         bits0 = bits_total - len(decimal)
         decimal = (bits0 * '0') + decimal
     return decimal
 
 def convert_bits_to_ip(bits):
+    '''
+    Convert String bits in ipv4 address notation
+    Used to convert 32 bits notation into IP
+    Arg:
+        bits: string bits notation
+    Return:
+        String ipv4 address notation
+    '''
     def base10(bits):
         if not bits:
             return ''
@@ -72,9 +118,23 @@ def convert_bits_to_ip(bits):
     return base10(bits).rstrip('.')
 
 def network_calc(ip, mask):
+    '''
+    Calculate network address of ip, mask addresses
+    Args:
+       ip: string bits notation
+       mask: string bits notation
+    Return: network string bits notation
+    '''
     return convert_decimal_to_bits(int(ip, base=2) & int(mask, base=2), bits_total=32)
 
 def broadcast_calc(mask, network):
+    '''
+    Calculate broadcast address of mask, network addresses
+    Args:
+       mask: string bits notation
+       network: string bits notation
+    Return: broadcast string bits notation
+    '''
     bits = mask.count('0')
     if bits > 0:
         broadcast = int(network, base=2) + int(bits * '1', base=2)
@@ -83,6 +143,13 @@ def broadcast_calc(mask, network):
         return network
 
 def hosts_calc(network, broadcast):
+    '''
+    Calculate every host inside the network
+    Args:
+       network: string bits notation
+       broadcast: string bits notation
+    Return: List of string bits notation of all hosts inside the network
+    '''
     hosts = set([network, broadcast])
     first, last = int(network, base=2), int(broadcast, base=2)
     for host in range(first, last):
@@ -90,6 +157,10 @@ def hosts_calc(network, broadcast):
     return hosts
 
 def print_ip_calc(ip):
+    '''
+    Calculate ip, mask, network, broadcast and print it out
+    Arg: ip - Receive string ip/cidr
+    '''
     ip, mask, network, broadcast = map(convert_bits_to_ip, ip_calc(ip))
     print('%-15s %-15s %-15s %-15s' % (ip, mask, network, broadcast))
 
